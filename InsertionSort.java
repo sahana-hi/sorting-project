dc1dddf7a3fadf2873d0451557c1dbd9e2526fd6
sahana-hi <sahana.venkatr@gmail.com>
2025-01-28 03:15:52 +0000

    insertion and selection finished

--- InsertionSort.java
+++ InsertionSort.java
@@ -1,6 +1,31 @@
 public class InsertionSort implements Sorter {
 
+    // public static void main(String[] args) {
+
+    //     InsertionSort s = new InsertionSort();
+    //     int[] testInput = { 1, 2, 3, 1 };
+    //     s.sort(testInput);
+    //     for(int i = 0; i < testInput.length; i++) {
+    //         System.out.print(testInput[i] + ", ");
+    //     }
+    // }
+
+
     public void sort(int[] input) {
         System.out.println("Insertion Sort!!!");
+
+        int n = input.length;
+        for (int i = 1; i < n; i++) {
+            int value = input[i];
+            int hole = i;
+
+            // insert the small values into the left 
+            while (hole > 0 && input[hole-1] > value) {
+                input[hole] = input[hole-1];
+                hole = hole - 1;
+            }
+            input[hole] = value;
+        }
+
     }
 }
--- SelectionSort.java
+++ SelectionSort.java
@@ -1,20 +1,32 @@
 public class SelectionSort implements Sorter {
 
+    // public static void main(String[] args) {
+
+    //     SelectionSort s = new SelectionSort();
+    //     int[] testInput = { 1, 2, 3, 1 };
+    //     s.sort(testInput);
+    //     for(int i = 0; i < testInput.length; i++) {
+    //         System.out.print(testInput[i] + ", ");
+    //     }
+    // }
+
     public void sort(int[] input) {
         System.out.println("Selection Sort!!!");
+        int n = input.length;
 
-        for (int i = 0; i < input.length -1; i++) {
+        // outer loop 
+        for (int i = 0; i < n - 1; i++) {
             int smallIndex = i;
-            // find smallest index after i
-            for (int j = i+1; j < input.length; j++) {
-                if (input[j] < smallIndex) {
+            // find index of smallest value after i
+            for (int j = i+1; j < n; j++) {
+                if (input[j] < input[smallIndex]) {
                     smallIndex = j;
                 }
             }
-            // swap i and smallestIndex values
-            int temp = input[i];
-            input[i] = input[smallIndex];
-            input[smallIndex] = temp;
+            // swap values in the index i and index smallestIndex 
+            int temp = input[smallIndex];
+            input[smallIndex] = input[i]; 
+            input[i] = temp;
         }
 
     }
