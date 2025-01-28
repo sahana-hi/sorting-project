4bdf978cf4d43a3bd4c000aef91fdc45b630ac20
sahana-hi <sahana.venkatr@gmail.com>
2025-01-28 02:35:25 +0000

    selection sort done

--- SelectionSort.java
+++ SelectionSort.java
@@ -2,5 +2,20 @@ public class SelectionSort implements Sorter {
 
     public void sort(int[] input) {
         System.out.println("Selection Sort!!!");
+
+        for (int i = 0; i < input.length -1; i++) {
+            int smallIndex = i;
+            // find smallest index after i
+            for (int j = i+1; j < input.length; j++) {
+                if (input[j] < smallIndex) {
+                    smallIndex = j;
+                }
+            }
+            // swap i and smallestIndex values
+            int temp = input[i];
+            input[i] = input[smallIndex];
+            input[smallIndex] = temp;
+        }
+
     }
 }
